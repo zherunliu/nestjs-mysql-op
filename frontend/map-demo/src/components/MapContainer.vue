7f5b8042c10018e96fbcb784e57437fe28676baf
zherunliu <1437257281@qq.com>
2026-01-05 15:22:34 +0800

    build: add map dependencies and env config

---
+++ frontend/map-demo/src/components/MapContainer.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from "vue";
+import AMapLoader from "@amap/amap-jsapi-loader";
+
+let map: any = null;
+
+onMounted(() => {
+  AMapLoader.load({
+    key: import.meta.env.VITE_JS_API_KEY,
+    version: "1.4.15",
+    plugins: [],
+  })
+    .then((AMap) => {
+      map = new AMap.Map("container", {
+        viewMode: "3D",
+        zoom: 11,
+        center: [116.397428, 39.90923],
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
+
+onUnmounted(() => {
+  map?.destroy();
+});
+</script>
+
+<template>
+  <div id="container"></div>
+</template>
+
+<style scoped>
+#container {
+  width: 100vw;
+  height: 800px;
+}
+</style>
